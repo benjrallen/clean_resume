8917b0dbb3de426b0244f8f345fa061b3ee97a96
Benjamin Allen <benjrallen@gmail.com>
2012-04-10 21:20:01 -0500

    faviconed

--- wp-content/themes/resume/header.php
+++ wp-content/themes/resume/header.php
@@ -25,6 +25,7 @@
 			wp_title( '|', true, 'right' );
 		?></title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="shortcut icon"  href="<?php bloginfo( 'url' ); ?>/favicon.png">
 		<meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE" /> 
 		<link rel="profile" href="http://gmpg.org/xfn/11" />
 		<link rel="stylesheet" href="<?php bloginfo( 'template_directory' ); ?>/css/application.css?v=2" />
